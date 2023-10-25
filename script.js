7c245827339537318599be98f72e3841395ad8e7
yaraano <qwertystar24@gmail.com>
2023-10-26 00:49:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+//
+// let a = 4
+//
+// let b = 55
+//
+// console.log(a + b)
+
+// let a = 4
+//
+// let b = 55
+//
+// console.log(a * b)
+
+// let a = 4
+//
+// let b = 55
+//
+// console.log(a / b)
+
+// let a = 4
+//
+// let b = 55
+//
+// console.log(a % b)
+
+// let a = 5
+//
+// let b = 'five'
+//
+// console.log(a * b);
+
+// let a = 5
+//
+// let b = '5'
+//
+// console.log(a + +b)
+
+let c = 2
+
+let b
+
+console.log(b)
